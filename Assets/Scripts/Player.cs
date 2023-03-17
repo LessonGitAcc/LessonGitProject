86fb63d7843c35a8998dd4eebcdcc1aa966e1bb9
Dmitry <lesson-git@mail.ru>
2023-03-17 20:56:49 +0300

    Добавил управление для игрока

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 4;
+    private Vector2 _move;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _move.x = Input.GetAxisRaw("Horizontal");
+        _move.y = Input.GetAxisRaw("Vertical");
+        _move.Normalize();
+        transform.Translate(_move * _speed * Time.deltaTime);
     }
 }
