3a584cf522a8ef4df63bfa893372209de5cf830f
Dmitry <lesson-git@mail.ru>
2023-03-17 21:06:45 +0300

    Добавил стрельбу и сделал префаб для патрона

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 4;
+    [SerializeField] private GameObject _bullet;
     private Vector2 _move;
 
     // Start is called before the first frame update
@@ -19,6 +20,14 @@ public class Player : MonoBehaviour
         _move.x = Input.GetAxisRaw("Horizontal");
         _move.y = Input.GetAxisRaw("Vertical");
         _move.Normalize();
-        transform.Translate(_move * _speed * Time.deltaTime);
+        Vector3 move = new Vector3(_move.x, 0, _move.y);
+        transform.Translate(move * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(_bullet, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.Impulse);
+            Destroy(bullet, 5);
+        }
     }
 }
